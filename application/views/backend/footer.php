f6dd175437b97a90e7714d1c0833c5f621394cb3
5degree Creative <5degreecreative@gmail.com>
2015-03-20 16:36:13 +0500

    no message

---
+++ application/views/backend/footer.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; 2015 <strong>5degree</strong>. 
+    Developed by 
+	<a href="http://www.5degree.com" 
+    	target="_blank">5degree Creative</a>
+</footer>
